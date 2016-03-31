683841bf29f7a8cde2cef052c9302e7102edd428
Isabelle Klein <Isa@Isabelles-Air.fritz.box>
2016-03-31 12:54:05 +0200

    Line Chart
    Visualisierung des Stromverlaufes

--- src/main/java/Waermepumpen_Controll.java
+++ src/main/java/Waermepumpen_Controll.java
@@ -1,8 +1,36 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Waermepumpen_Controll {
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+
+public class Waermepumpen_Controll extends ApplicationFrame {
+	final static DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	final static String series1 = "aktueller Verbrauch";
+	
 	public static void main(String[] args) {
+		
+		/*
+		 * Line Chart
+		 */
+        final Waermepumpen_Controll demo = new Waermepumpen_Controll("Line Chart Demo");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+		
 		int anzahl_pumpen = 100;
 
 		List<Waermepumpe> pumpen_hd = new LinkedList<>();
@@ -54,11 +82,109 @@ public class Waermepumpen_Controll {
 			stromgraph.add(s);
 		}
 
-		/*for (aktueller_Strom st : stromgraph) {
-		*	System.out.println(st.getTime() + " Strom: " + st.getStrom() + " kW" );
-		*}
-		*/
+		
+		for (aktueller_Strom st : stromgraph) {
+		//	System.out.println(st.getTime() + " Strom: " + st.getStrom() + " kW" );
+			dataset.addValue(st.getStrom(), series1, Double.toString(st.getTime()));
+		}
+		
+		
 		
 
 	}
+	
+	/*
+	 * Line Chart
+	 */
+	
+	 public Waermepumpen_Controll (final String title) {
+	        super(title);
+	        //final CategoryDataset dataset1 = dataset;
+	        final JFreeChart chart = createChart(dataset);
+	        final ChartPanel chartPanel = new ChartPanel(chart);
+	        chartPanel.setPreferredSize(new Dimension(500, 270));
+	        setContentPane(chartPanel);
+	    }
+
+	    
+	    /**
+	     * Creates a sample chart.
+	     * 
+	     * @param dataset  a dataset.
+	     * 
+	     * @return The chart.
+	     */
+	    private JFreeChart createChart(final CategoryDataset dataset) {
+	        
+	        // create the chart...
+	        final JFreeChart chart = ChartFactory.createLineChart(
+	            "Stromauslastung",       // chart title
+	            "Zeit",                    // domain axis label
+	            "kWh",                   // range axis label
+	            dataset,                   // data
+	            PlotOrientation.VERTICAL,  // orientation
+	            true,                      // include legend
+	            true,                      // tooltips
+	            false                      // urls
+	        );
+
+	        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+//	        final StandardLegend legend = (StandardLegend) chart.getLegend();
+	  //      legend.setDisplaySeriesShapes(true);
+	    //    legend.setShapeScaleX(1.5);
+	      //  legend.setShapeScaleY(1.5);
+	        //legend.setDisplaySeriesLines(true);
+
+	        chart.setBackgroundPaint(Color.white);
+
+	        final CategoryPlot plot = (CategoryPlot) chart.getPlot();
+	        plot.setBackgroundPaint(Color.lightGray);
+	        plot.setRangeGridlinePaint(Color.white);
+
+	        // customise the range axis...
+	        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+	        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+	        rangeAxis.setAutoRangeIncludesZero(true);
+
+	        // ****************************************************************************
+	        // * JFREECHART DEVELOPER GUIDE                                               *
+	        // * The JFreeChart Developer Guide, written by David Gilbert, is available   *
+	        // * to purchase from Object Refinery Limited:                                *
+	        // *                                                                          *
+	        // * http://www.object-refinery.com/jfreechart/guide.html                     *
+	        // *                                                                          *
+	        // * Sales are used to provide funding for the JFreeChart project - please    * 
+	        // * support us so that we can continue developing free software.             *
+	        // ****************************************************************************
+	        
+	        // customise the renderer...
+	        final LineAndShapeRenderer renderer = (LineAndShapeRenderer) plot.getRenderer();
+//	        renderer.setDrawShapes(true);
+
+	        renderer.setSeriesStroke(
+	            0, new BasicStroke(
+	                2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND,
+	                1.0f, new float[] {10.0f, 6.0f}, 0.0f
+	            )
+	        );
+	        renderer.setSeriesStroke(
+	            1, new BasicStroke(
+	                2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND,
+	                1.0f, new float[] {6.0f, 6.0f}, 0.0f
+	            )
+	        );
+	        renderer.setSeriesStroke(
+	            2, new BasicStroke(
+	                2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND,
+	                1.0f, new float[] {2.0f, 6.0f}, 0.0f
+	            )
+	        );
+	        // OPTIONAL CUSTOMISATION COMPLETED.
+	        
+	        return chart;
+	    }
+	    
+
+
+	
 }
